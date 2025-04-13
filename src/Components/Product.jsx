176e8979f1aa0121f300df5cc29c83c9dd21cebd
kabeerthenix <kabeerai9090@gmail.com>
2025-04-14 01:49:39 +0500

    Update Product.jsx

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -4,33 +4,22 @@ import image2 from './images/shirt 2.jpg';
 import image3 from './images/shirt 3.jpg';
 import image4 from './images/shirt 4.jpg';
 import image6 from './images/shirt 5.jpg';
-// import image7 from './images/polo7.jpg';
-// import image8 from './images/polo8.jpg';
-// import image9 from './images/print1.jpeg';
-// import image10 from './images/print2.png';
-// import image11 from './images/print3.jpeg';
-// import image12 from './images/print4.png';
-// import image13 from './images/print5.jpeg';
-// import image14 from './images/print6.png';
-// import image15 from './images/plain1.png';
-// import image16 from './images/plain2.png';
-// import image17 from './images/plain3.jpg';
-// import image18 from './images/plain4.png';
-// import image19 from './images/plain5.png';
-// import image20 from './images/polo10.jpg';
-
-
+import image7 from './images/shirt 6.jpg';
+import image8 from './images/shirt 7.jpg';
+import image20 from './images/shirt 8.jpg';
+import image9 from './images/shirt 9.jpg';
+import image10 from './images/shirt 10.jpg';
+import image11 from './images/shirt 11.jpg';
 
 
 export const products = [
-  // Polo T-shirts 
   {
     id: 1,
     name: 'Perfectly T-Shirt',
-    price: 1000,
+    price: 600,
     image: image1,
-    regularPrice: 1200,
-    salePrice: 1000,
+    regularPrice: 1000,
+    salePrice: 600,
     fit: 'Regular',
     showSizeOptions: true,
     description: 'A trendy printed T-shirt perfect for casual outings. Made from soft and breathable fabric, offering comfort and style.',
@@ -38,10 +27,10 @@ export const products = [
   {
     id: 2,
     name: 'City T-Shirt',
-    price: 1000,
+    price: 600,
     image: image2,
-    regularPrice: 1200,
-    salePrice: 1000,
+    regularPrice: 1000,
+    salePrice: 600,
     fit: 'Slim',
     showSizeOptions: true,
     description: 'A classic white polo shirt with a slim fit, designed to offer a clean and sophisticated look. Ideal for both casual and semi-formal events.',
@@ -49,10 +38,10 @@ export const products = [
   {
     id: 3,
     name: 'Style in Hush T-Shirt',
-    price: 1000,
+    price: 600,
     image: image3,
-    regularPrice: 1200,
-    salePrice: 1000,
+    regularPrice: 1000,
+    salePrice: 600,
     fit: 'Regular',
     showSizeOptions: true,
     description: 'A versatile light blue polo shirt with a regular fit. Perfect for summer, providing both comfort and a refreshing color.',
@@ -60,10 +49,10 @@ export const products = [
   {
     id: 5,
     name: 'Never give up T-Shirt',
-    price: 1000,
+    price: 600,
     image: image4,
-    regularPrice: 1200,
-    salePrice: 1000,
+    regularPrice: 1000,
+    salePrice: 600,
     fit: 'Slim',
     showSizeOptions: true,
     description: 'A stylish forest green polo shirt with a slim fit, perfect for adding a touch of nature-inspired color to your wardrobe.',
@@ -71,157 +60,74 @@ export const products = [
   {
     id: 6,
     name: 'Gold Blood T-Shirt',
-    price: 1000,
+    price: 600,
     image: image6,
-    regularPrice: 1200,
-    salePrice: 1000,
+    regularPrice: 1000,
+    salePrice: 600,
     fit: 'Slim',
     showSizeOptions: true,
     description: 'A refreshing aqua polo shirt with a slim fit. This shirt is perfect for a cool and casual look, ideal for warm weather.',
   },
-  
-  //   {
-  //     id: 7,
-  //     name: 'Dark Blue Polo Shirt',
-  //     price: 1000,
-  //     image: image7,
-  //     regularPrice: 1200,
-  //     salePrice: 1000,
-  //     fit: 'Slim',
-  //     showSizeOptions: true,
-  //   },
-  //   {
-  //     id: 8,
-  //     name: 'Lime Green Polo Shirt',
-  //     price: 1000,
-  //     image: image8,
-  //     regularPrice: 1200,
-  //     salePrice: 1000,
-  //     fit: 'Slim',
-  //     showSizeOptions: true,
-  //   },
+  {
+    id: 7,
+    name: 'EMotion Black T-Shirt',
+    price: 1000,
+    image: image7,
+    regularPrice: 1400,
+    salePrice: 1000,
+    fit: 'Slim',
+    showSizeOptions: true,
+  },
+  {
+    id: 8,
+    name: 'EMotion Lime Green T-Shirt',
+    price: 1000,
+    image: image8,
+    regularPrice: 1400,
+    salePrice: 1000,
+    fit: 'Slim',
+    showSizeOptions: true,
+  },
+  {
+    id: 20,
+    name: 'EMotion T-Shirt',
+    price: 1000,
+    image: image20,
+    regularPrice: 1400,
+    salePrice: 1000,
+    fit: 'Slim',
+    showSizeOptions: true,
+  },
+  {
+    id: 9,
+    name: '3 Diamonds Black T-Shirt',
+    price: 1000,
+    image: image9,
+    regularPrice: 1400,
+    salePrice: 1000,
+    fit: 'Slim',
+    showSizeOptions: true,
+  },
+  {
+    id: 10,
+    name: '3 Diamonds Grey T-Shirt',
+    price: 1000,
+    image: image10,
+    regularPrice: 1400,
+    salePrice: 1000,
+    fit: 'Slim',
+    showSizeOptions: true,
+  },
+  {
+    id: 11,
+    name: '3 Diamonds Mud T-Shirt',
+    price: 1000,
+    image: image11,
+    regularPrice: 1400,
+    salePrice: 1000,
+    fit: 'Slim',
+    showSizeOptions: true,
+  },
 
-  //   {
-  //     id: 20,
-  //     name: 'Slim Black Polo Shirt',
-  //     price: 1000,
-  //     image: image20,
-  //     regularPrice: 1200,
-  //     salePrice: 1000,
-  //     fit: 'Slim',
-  //     showSizeOptions: true,
-  //   },
-  //   // Printed t shirts 
-  //   {
-  //     id: 9,
-  //     name: 'Printed Grey T-Shirt',
-  //     price: 1000,
-  //     image: image9,
-  //     regularPrice: 1200,
-  //     salePrice: 1000,
-  //     fit: 'Regular',
-  //     showSizeOptions: true,
-  //   },
 
-  //   {
-  //     id: 10,
-  //     name: 'Printed Black T-Shirt',
-  //     price: 1000,
-  //     image: image10,
-  //     regularPrice: 1200,
-  //     salePrice: 1000,
-  //     fit: 'Regular',
-  //     showSizeOptions: true,
-  //   },
-  //   {
-  //     id: 11,
-  //     name: 'Printed White T-Shirt',
-  //     price: 1000,
-  //     image: image11,
-  //     regularPrice: 1200,
-  //     salePrice: 1000,
-  //     fit: 'Slim',
-  //     showSizeOptions: true,
-  //   },
-  //   {
-  //     id: 12,
-  //     name: 'Printed Blue T-Shirt',
-  //     price: 1000,
-  //     image: image12,
-  //     regularPrice: 1200,
-  //     salePrice: 1000,
-  //     fit: 'Slim',
-  //     showSizeOptions: true,
-  //   },
-  //   {
-  //     id: 13,
-  //     name: 'Printed Blue T-Shirt',
-  //     price: 1000,
-  //     image: image13,
-  //     regularPrice: 1200,
-  //     salePrice: 1000,
-  //     fit: 'Slim',
-  //     showSizeOptions: true,
-  //   },
-  //   {
-  //     id: 14,
-  //     name: 'Printed Blue T-Shirt',
-  //     price: 1000,
-  //     image: image14,
-  //     regularPrice: 1200,
-  //     salePrice: 1000,
-  //     fit: 'Slim',
-  //     showSizeOptions: true,
-  //   },
-    
-  // {
-  //   id: 15,
-  //   name: 'Red Solid Plain T-Shirt',
-  //   price: 800,
-  //   regularPrice: 1000,
-  //   salePrice: 800,
-  //   image: image15,
-  // showSizeOptions: true,
-
-  // },
-  // {
-  //   id: 16,
-  //   name: 'Focus Pink Solid Plain T-Shirt',
-  //   price: 800,
-  //   regularPrice: 1000,
-  //   salePrice: 800,
-  //   image: image16,
-  // showSizeOptions: true,
-    
-  // },
-  // {
-  //   id: 17,
-  //   name: 'Forest Green Solid Plain T-Shirt',
-  //   price: 800,
-  //   regularPrice: 1000,
-  //   salePrice: 800,
-  //   image: image17,
-  // showSizeOptions: true,
-  
-  // },
-  // {
-  //   id: 18,
-  //   name: 'Pink Solid Plain T-Shirt',
-  //   price: 800,
-  //   regularPrice: 1000,
-  //   salePrice: 800,
-  //   image: image18,
-  // showSizeOptions: true,
-
-  // },
-  // {
-  //   id: 19,
-  //   name: 'Aqua Solid Plain T-Shirt',
-  //   price: 800,
-  //   regularPrice: 1000,
-  //   salePrice: 800,
-  //   image: image19,
-  // showSizeOptions: true,
- 
-  // },
-  ]
+];
